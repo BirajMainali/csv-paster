3efb2b1857d9ea1fa889d1e96ed776e8d05a53d0
BirajMainali <biraj.prathamit@gmail.com>
2022-08-29 21:27:56 +0545

    feat: fill sheet

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ document.addEventListener('paste', (ev) => {
     rows = Array.from(rows).filter((row, index) => index >= currentRowIndex);
     const values = ev.clipboardData.getData("text/plain").split("\n").map(row => row.split('\t'));
     for (let rowIdx = 0; rowIdx < values.length; rowIdx++) {
+        if (rowIdx >= rows.length) {
+            const newRow = currentRow.cloneNode(true);
+            currentRow.parentNode.appendChild(newRow);
+            rows.push(newRow);
+        }
         let cells = Array.from(rows[rowIdx].cells).filter((cell, cellIdx) => cellIdx >= currentCellIndex);
         cells = Array.from(cells).filter(cell => !cell.classList.contains('skip-import'));
         for (let cellIdx = 0; cellIdx < cells.length; cellIdx++) {
