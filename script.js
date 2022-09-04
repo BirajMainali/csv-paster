a29aaef33f4420e96af77526f69bc5f471e29c15
BirajMainali <biraj.prathamit@gmail.com>
2022-09-04 21:18:22 +0545

    chore: handle more inputes

--- script.js
+++ script.js
@@ -1,32 +1,89 @@
+let CURRENT_TRACE = null;
+
+const getclipboardDataValues = (ev) => {
+    const values = ev.clipboardData.getData("text/plain").split("\n").map(row => row.split('\t'));
+    values.splice(values.length - 1, 0);
+    return values;
+}
+
+
+const getSerialized = (tableElem) => {
+    const rows = Array.from(tableElem.querySelectorAll('tbody tr')).filter(row => row.dataset.trace === CURRENT_TRACE);
+    const tableHeaders = Array.from(tableElem.querySelectorAll('th')).map(x => x.dataset.header).filter(x => x);
+    return Array.from(rows).map(row => {
+        const cells = Array.from(row.querySelectorAll('td')).filter(x => x.querySelector('input') || x.querySelector('select'));
+        return cells.reduce((acc, cur, index) => {
+            acc[tableHeaders[index]] = getCurrentValue(cur);
+            return acc;
+        }, {})
+    })
+}
+
+const getCurrentValue = (currElem) => {
+    const input = currElem.querySelector('input');
+    if (input) return input.value;
+    const selectElem = currElem.querySelector('select');
+    if (selectElem) return selectElem.value;
+}
+
+const setCurrentValue = (currentElem, data) => {
+    let inputElem = null;
+    inputElem = currentElem.querySelector('input');
+    if (inputElem) {
+        if (inputElem.type === 'checkbox' || inputElem.type === 'radio') {
+            inputElem.checked = (data == 1);
+            inputElem.value = (data == 1);
+        } else {
+            inputElem.value = data;
+        }
+    } else {
+        inputElem = currentElem.querySelector('select');
+        if (inputElem) inputElem.value = data;
+    }
+    if (!inputElem.closest('td').classList.contains('watch')) return;
+    inputElem.closest('tr').dataset.trace = CURRENT_TRACE;
+}
+
+const getRow = (currentElem) => {
+    const newRow = currentElem.cloneNode(true);
+    currentElem.parentNode.appendChild(newRow);
+    return newRow;
+}
+
+const dispatchEvent = (elem, eventName, data) => {
+    if (!data) return;
+    console.log(data);
+    const event = new CustomEvent(eventName, {
+        bubbles: true,
+        detail: data,
+    });
+    elem.dispatchEvent(event);
+};
+
 document.addEventListener('paste', (ev) => {
     ev.preventDefault();
-    if (!ev.target.closest("table")) return;
-    let rows = document.querySelectorAll('table tbody tr');
-    const currentRow = ev.target.closest('tbody tr');
-    const currentRowIndex = Array.from(rows).indexOf(currentRow);
-    const currentCell = ev.target.closest('td');
-    const currentCellIndex = Array.from(currentRow.children).indexOf(currentCell);
-    rows = Array.from(rows).filter((row, index) => index >= currentRowIndex);
-    const values = ev.clipboardData.getData("text/plain").split("\n").map(row => row.split('\t'));
-    for (let rowIdx = 0; rowIdx < values.length ; rowIdx++) {
-        if (rowIdx >= rows.length) {
-            const newRow = currentRow.cloneNode(true);
-            currentRow.parentNode.appendChild(newRow);
-            rows.push(newRow);
+    const tableElem = ev.target.closest('#csv');
+    if (!tableElem) return;
+    const csvFormattedValues = getclipboardDataValues(ev);
+    dispatchEvent(tableElem, 'onRows', csvFormattedValues.length);
+    setTimeout(() => {
+        CURRENT_TRACE = Math.random().toString(36).substring(7);
+        let rows = document.querySelectorAll('#csv tbody tr');
+        const currentRow = ev.target.closest('tbody tr');
+        const currentRowIndex = Array.from(rows).indexOf(currentRow);
+        const currentCell = ev.target.closest('td');
+        const currentCellIndex = Array.from(currentRow.children).indexOf(currentCell);
+        rows = Array.from(rows).filter((row, index) => index >= currentRowIndex);
+        for (let rowIdx = 0; rowIdx < csvFormattedValues.length; rowIdx++) {
+            if (rowIdx >= rows.length) rows.push(getRow(currentRow));
+            let cells = Array.from(rows[rowIdx].cells).filter((cell, cellIdx) => cellIdx >= currentCellIndex);
+            cells = Array.from(cells).filter(cell => !cell.classList.contains('skip-paste'));
+            for (let cellIdx = 0; cellIdx < cells.length; cellIdx++) {
+                const value = csvFormattedValues[rowIdx][cellIdx];
+                if (!value) continue;
+                setCurrentValue(cells[cellIdx], value);
+            }
         }
-        let cells = Array.from(rows[rowIdx].cells).filter((cell, cellIdx) => cellIdx >= currentCellIndex);
-        cells = Array.from(cells).filter(cell => !cell.classList.contains('skip-import'));
-        for (let cellIdx = 0; cellIdx < cells.length; cellIdx++) {
-            const input = cells[cellIdx].querySelector('input');
-            const value = values[rowIdx][cellIdx];
-            if(!value) continue;
-            if (!input) continue;
-            input.value = value;
-        }
-    }
-});
-
-document.querySelector('.btn').addEventListener('click', (ev) => {
-    const inputs = document.querySelectorAll('table tbody tr td input');
-    inputs.forEach(input => input.value = '');
+        dispatchEvent(tableElem, 'pasteComplete', getSerialized(tableElem));
+    }, 0);
 });
